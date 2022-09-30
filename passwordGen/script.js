b04f8b901503d989bb490553144d87795e2c2add
JCool221 <109809337+JCool221@users.noreply.github.com>
2022-09-30 12:22:08 -0400

    repo

---
+++ passwordGen/script.js
@@ -0,0 +1,36 @@
+// Assignment Code
+
+// generate a string of 16 random numbers
+// add event to ask for types of characters
+// replace the random string of numbers with the alpha-numerics and symbols
+// print the final password to the textbox
+
+var generateBtn = document.querySelector("#generate");
+var lowerCase = 'abcdefghijklmnopqrstuvwxyz';
+var upperCase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+var numbers = '0123456789';
+var symbols = '`~!@#$%^&*()_-+=/?.>,<;:'
+
+function generatePassword() {
+    var passwd = '';
+    var chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+    for (i=1;i<16;i++) {
+      var c = Math.floor(Math.random()*chars.length + 1);
+      passwd += chars.charAt(c)
+    }
+  
+    return passwd;
+  
+  }
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
